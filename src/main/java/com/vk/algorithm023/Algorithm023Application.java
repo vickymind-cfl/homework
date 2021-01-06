1049dfc80a00e18775c997330e7682a3e69a65eb
vickymind <vickymind@163.com>
2021-01-06 18:20:55 +0800

    构建项目

---
+++ src/main/java/com/vk/algorithm023/Algorithm023Application.java
@@ -0,0 +1,13 @@
+package com.vk.algorithm023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Algorithm023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Algorithm023Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/vk/algorithm023/Algorithm023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vk.algorithm023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Algorithm023ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
